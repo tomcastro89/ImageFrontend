762e7dfd1d17d4e70ad8f2d3932ebf1b3d0b2cfe
Tommaso Castrovillari <tcastrovillari@itemis.de>
2019-07-16 15:55:50 +0200

    Step 7: Implement the state of the ImageList component has a list called images

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import {GridList} from "@material-ui/core";
-export default class ImageList extends React.Component {
+
+interface State {
+    images: []
+}
+interface Props {}
+
+export default class ImageList extends React.Component<Props,State> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            images: []
+        }
+    }
     render() {
         return <GridList/>;
     }
