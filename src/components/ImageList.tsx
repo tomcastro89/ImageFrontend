67de20b1b0d9fdbd0ee9190f8428c6931236742a
Tommaso Castrovillari <tcastrovillari@itemis.de>
2019-07-16 15:49:25 +0200

    Step 5: Let the ImageList Component return a GridList Component

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import {GridList} from "@material-ui/core";
 export default class ImageList extends React.Component {
     render() {
-        return <div/>;
+        return <GridList/>;
     }
 }
